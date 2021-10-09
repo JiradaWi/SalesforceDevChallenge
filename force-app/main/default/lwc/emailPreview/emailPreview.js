2603f73294bb554ddd4ddde842d4084f935d8c25
jiradawi <cutiemoon08@yahoo.com>
2021-10-09 20:01:03 +0700

    first commit

---
+++ force-app/main/default/lwc/emailPreview/emailPreview.js
@@ -0,0 +1,5 @@
+import { LightningElement, track } from 'lwc';
+
+import getCampaign from '@salesforce/apex/emailPreviewController.getCampaign';
+
+export default class EmailPreview extends LightningElement {}
