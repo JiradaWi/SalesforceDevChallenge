0f888c67f9c84c58e175b3c2badb0a98d8c7c852
jiradawi <cutiemoon08@yahoo.com>
2021-10-10 13:11:35 +0700

    page outline

--- force-app/main/default/lwc/emailPreview/emailPreview.js
+++ force-app/main/default/lwc/emailPreview/emailPreview.js
@@ -1,5 +1,44 @@
-import { LightningElement, track } from 'lwc';
+import { LightningElement, track, wire } from 'lwc';
+import { CurrentPageReference } from "lightning/navigation";
 
 import getCampaign from '@salesforce/apex/emailPreviewController.getCampaign';
 
-export default class EmailPreview extends LightningElement {}
+export default class EmailPreview extends LightningElement {
+
+    @track campaignId;  
+    @track campaignName;
+
+    @wire(CurrentPageReference)
+    getStateParameters(currentPageReference) {
+        if (currentPageReference) {
+            const urlValue = currentPageReference.state.c__id;
+            console.log('urlValue: '+urlValue);
+            if (urlValue) {
+                this.campaignId = urlValue;
+            } else {
+                this.campaignId = `URL Value was not set`;
+            }
+        }
+    }
+    
+    connectedCallback(){
+        // const campaignId = this.getStateParameters();
+        this.getCampaignInfo();
+     }
+
+     getCampaignInfo(){
+         console.log('getCampaign');
+
+        getCampaign({campaignId: this.campaignId})
+        .then( result=>{               
+                this.campaignName = result.Campaign.Name;              
+            }
+        ).catch(error=>{
+
+        })
+     }
+
+     toCampaign(){
+         
+     }
+}
