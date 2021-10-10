01061a910acdc15d6283261f607f21369bd6736a
jiradawi <cutiemoon08@yahoo.com>
2021-10-10 13:50:05 +0700

    Navigate back to campaign and outline on template

--- force-app/main/default/lwc/emailPreview/emailPreview.js
+++ force-app/main/default/lwc/emailPreview/emailPreview.js
@@ -1,44 +1,67 @@
 import { LightningElement, track, wire } from 'lwc';
 import { CurrentPageReference } from "lightning/navigation";
+import { NavigationMixin } from 'lightning/navigation';
+import { ShowToastEvent } from 'lightning/platformShowToastEvent'
 
 import getCampaign from '@salesforce/apex/emailPreviewController.getCampaign';
 
-export default class EmailPreview extends LightningElement {
+export default class EmailPreview extends NavigationMixin(LightningElement) {
 
-    @track campaignId;  
+    @track campaignId;
     @track campaignName;
 
     @wire(CurrentPageReference)
     getStateParameters(currentPageReference) {
         if (currentPageReference) {
             const urlValue = currentPageReference.state.c__id;
-            console.log('urlValue: '+urlValue);
             if (urlValue) {
                 this.campaignId = urlValue;
             } else {
-                this.campaignId = `URL Value was not set`;
+                this.showToast('No Campaign ID Found!', 'Please navigate to this page using campaign record page.', 'error');
             }
         }
     }
-    
-    connectedCallback(){
+
+    connectedCallback() {
         // const campaignId = this.getStateParameters();
         this.getCampaignInfo();
-     }
+    }
 
-     getCampaignInfo(){
-         console.log('getCampaign');
+    getCampaignInfo() {
+        console.log('getCampaign');
+        if (this.campaignId) {
+            getCampaign({ campaignId: this.campaignId})
+                .then(result => {
+                    this.campaignName = result.Campaign.Name;
+                }
+                ).catch(error => {
+                    console.log(JSON.stringify(error));
+                    this.showToast('Campaign ID Incorrect!', 'Please navigate to this page using campaign record page.', 'error');
+                })
+        }
+    }
 
-        getCampaign({campaignId: this.campaignId})
-        .then( result=>{               
-                this.campaignName = result.Campaign.Name;              
-            }
-        ).catch(error=>{
+    showToast(title, message, variant) {
+        const event = new ShowToastEvent({
+            title: title,
+            message: message,
+            variant: variant
+        });
+        this.dispatchEvent(event);
+    }
 
-        })
-     }
 
-     toCampaign(){
-         
-     }
+    toCampaign() {
+        console.log('test');
+
+        this[NavigationMixin.Navigate]({
+            type: 'standard__recordPage',
+            attributes: {
+                recordId: this.campaignId,
+                objectApiName: 'Campaign',
+                actionName: 'view'
+            },
+        });
+        console.log('test2');
+    }
 }
